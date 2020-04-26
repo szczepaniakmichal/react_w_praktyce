e559936a114e1976c680ced75447109c6346aec9
szczepaniakmichal <mssk87@gmail.com>
2020-04-26 16:34:49 +0200

    new structure / change App to Root / set absolute imports

---
+++ src/views/Root/Root.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from 'components/Button/Button';
+
+const Root = () => (
+  <div className="App">
+    <Button width="300px">save / close</Button>
+    <Button secondBtn>remove</Button>
+  </div>
+);
+
+export default Root;
